4e12b3a50cb4d32e3e25b71503355d37a61f7f76
DiegoVaitcunas123 <dv289389@fi365.ort.edu.uy>
2023-07-31 20:34:08 -0300

    SE AGREGARON NUEVAS FUNCIONALIDAD
    FUNCIONES DE LOGIN Y REGISTRO

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1,3 +1,4 @@
+////////////////////////////GENERALIDADES/////////////////////////////////////////
 const MENU = document.querySelector('#menu');
 const ROUTER = document.querySelector('#ruteo');
 const HOME = document.querySelector('#pantalla-home');
@@ -5,6 +6,7 @@ const LOGIN = document.querySelector('#pantalla-login');
 const REGISTRO = document.querySelector('#pantalla-registro');
 const REGISTRAR_CENSO = document.querySelector('#pantalla-registros');
 /*const MAPA = document.querySelector('#menu');*/
+
 
 function cerrarMenu(){
     MENU.close();
@@ -41,3 +43,129 @@ function ocultarPantallas(){
     REGISTRO.style.display = 'none';
     REGISTRAR_CENSO.style.display = 'none';
 }
+
+let personas = [];
+
+let tokenUsuarioLogueado = '';
+
+let baseURL = 'https://censo.develotion.com/'
+
+inicializar();
+
+function inicializar() {
+    suscripcionAEventos()
+    console.log('Comienza la app')
+}
+
+function suscripcionAEventos() {
+    // Login.
+    document.querySelector("#btnLoginIngresar").addEventListener("click", btnLoginIngresarHandler);
+    // Registro.
+    //document.querySelector("#btnRegistroRegistrarse").addEventListener("click", btnRegistroRegistrarseHandler);
+    // Ruteo
+    ROUTER.addEventListener("ionRouteDidChange", navegar);
+    // Sucursales
+    //COMBO_FILTRO_SUCURSALES.addEventListener("ionChange", comboSucursalesChangeHandler);
+}
+
+
+
+
+function actualizarMenu() {
+    // Oculto todo, luego mostraré sólo lo que corresponde.
+    document.querySelector("#btnMenuIngreso").style.display = "none";
+    document.querySelector("#btnMenuRegistro").style.display = "none";
+    document.querySelector("#btnMenuCerrarSesion").style.display = "none";
+    if (tokenUsuarioLogueado) {
+        document.querySelector("#btnMenuCerrarSesion").style.display = "block";  
+    } else {
+        //document.querySelector("#btnMenuIngreso").style.display = "block";
+        //document.querySelector("#btnMenuRegistro").style.display = "block";
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////// LOGIN ////////////////////////////////////////////////
+
+function btnLoginIngresarHandler () {
+    const emailIngresado = document.querySelector("#txtLoginEmail").value;
+    const passwordIngresado = document.querySelector("#txtLoginPassword").value;
+    console.log("Comienzo a registrar un usuario");
+    if (emailIngresado.trim().length > 0 && passwordIngresado.trim().length > 0) {
+        let datos = {
+            email: emailIngresado,
+            password: passwordIngresado
+        };
+        
+        fetch(baseURL + 'login.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datos)
+            
+        })
+        .then(response => {
+            console.log("LLego al then", response);
+            return response.json();
+        })
+        .then(data => {
+            if (data.error) {
+                mostrarToast('ERROR', 'Error', data.error);
+            }
+        })
+        .catch(error => console.log(error));
+    } else {
+        mostrarToast('ERROR', 'Datos incompletos', 'Debe ingresar email y contraseña');
+    }
+    console.log("Llego hasta aca, final de la funcion");
+}
+
+async function mostrarToast(tipo, titulo, mensaje) {
+    const toast = document.createElement('ion-toast');
+    toast.header = titulo;
+    toast.message = mensaje;
+    toast.position = 'bottom';
+    toast.duration = 2000;
+    if (tipo === "ERROR") {
+        toast.color = "danger";
+    } else if (tipo === "SUCCESS") {
+        toast.color = "success";
+    } else if (tipo === "WARNING") {
+        toast.color = "warning";
+    }
+
+    document.body.appendChild(toast);
+    return toast.present();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////// REGISTRO /////////////////////////////////////////////
+function vaciarCamposRegistro() {
+    document.querySelector("#txtRegistroNombre").value = '';
+    document.querySelector("#txtRegistroApellido").value = '';
+    document.querySelector("#txtRegistroEmail").value = '';
+    document.querySelector("#txtRegistroDireccion").value = '';
+    document.querySelector("#txtRegistroPassword").value = '';
+    document.querySelector("#txtRegistroVerificacionPassword").value = '';
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////// REGISTRO CENSO ///////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////// LISTAR CENSO /////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
